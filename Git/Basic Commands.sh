4feb6b0e180c2a64fb7b015928d62cacec7b85ce
Archit Awasthi <143782101+archit-awasthi@users.noreply.github.com>
2025-11-11 22:48:48 +0530

    Add basic Git commands to script

---
+++ Git/Basic Commands.sh
@@ -0,0 +1,48 @@
+  1  git --versionj
+    2  git version
+    3  whoami
+    4  git config --global user.name "archit"
+    5  whoami
+    6  git config --global user.email "architawasthims7@gmail.com"
+    7  git config --global user.name
+    8  git config --global user.email
+    9  pwd
+   10  mkdir git1_demo
+   11  cd git1_demo
+   12  touch demo1.txt
+   13  nano demo1.txt
+   14  echo demo1.txt
+   15  echo "hello i am archit"
+   16  touch hello.c
+   17  nano hello.c
+   18  ./hello.c
+   19  rm hello.c
+   20  ls
+   21  git inti
+   22  git init
+   23  git add demo1.txt
+   24  git add demo1.txt
+   25  git clone https://github.com/archit-awasthi/Student-Quotes/blob/main/monday-quote.txt
+   26  git clone https://github.com/archit-awasthi/Student-Quotes/blob/main/monday-quote.txt
+   27  git clone https://github.com/archit-awasthi/Student-Quotes.git
+   28  ls
+   29  git commit -m "hello i am archit."
+   30  ls
+   31  cd Student-Quotes
+   32  ls
+   33  nano monday-quote.txt
+   34  git push origin main
+   35  git push origin main
+   36  cd ..
+   37  ls
+   38  cd Student-Quotes
+   39  git push origin main
+   40  ls
+   41  nano monday-quote.txt
+   42  nano monday-quote.txt
+   43  git status
+   44  git add monday-quote.txt
+   45  git push origin main
+   46  git commit -m "i have updated the monday file....."
+   47  git push origin main
+   48  history
